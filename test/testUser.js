7c31c6151083ef79334c1f5f43ec92bed0945591
jerrykid <ngdinhthi@gmail.com>
2016-03-06 11:22:14 +0700

    Initial

---
+++ test/testUser.js
@@ -0,0 +1,18 @@
+﻿var user = require('../user/user.js');
+var should = require('should');
+var done = {};
+
+describe('User', function () {
+	describe('Should initial user', function () {
+
+		it('Should initial user', function () {
+			should.exist(user);
+		})
+
+		it('Should get user successfull', function () {
+			console.log(user);
+			user.getUser(done);
+		});
+				
+	})
+});	
